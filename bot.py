818f2a729b3bb42fabb20a5e0ad3ed64aaf07b8b
devkrytoy4el <50165574+devkrytoy4el@users.noreply.github.com>
2021-03-19 09:03:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def get_text_messages(message):
                 marks[i] = int(marks[i])
             ans = ('Средний балл равен - ' + str(round((sum(marks) / len(marks)),4)))
             bot.send_message(message.from_user.id, ans )
-            print ('[done] -',str(ans))
+            print ('[done] -',str(ans), message.from_user.first_name)
         except:
             bot.send_message(message.from_user.id, 'Попробуйте ещё раз' )
 bot.polling()
