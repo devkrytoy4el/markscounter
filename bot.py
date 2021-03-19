ee1fbf315bd1832b027d89e25b27707eb1239d1d
devkrytoy4el <50165574+devkrytoy4el@users.noreply.github.com>
2021-03-19 19:14:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,14 +13,33 @@ def get_text_messages(message):
     elif message.text == "/start":
         bot.send_message(message.from_user.id, "Просто введи свои оценки")
     else :
-        marks = list(message.text.replace(" ","").replace(",","").replace(";","").replace(":","").replace(".",""))
-       
+        marks = list(message.text.replace(" ","").replace(",","").replace(".",""))
         try:
             for i in range(len(marks)):
                 marks[i] = int(marks[i])
-            ans = ('Средний балл равен - ' + str(round((sum(marks) / len(marks)),4)))
+            a = str(round((sum(marks) / len(marks)),4))
+            ans = ('Средний балл равен:')
             bot.send_message(message.from_user.id, ans )
             print ('[done] -',str(ans), 'by', message.from_user.username)
+
+
         except:
             bot.send_message(message.from_user.id, 'Попробуйте ещё раз' )
+        if True:
+            image = Image.open("fol/images.jpg")
+            font = ImageFont.truetype("Circe-ExtraLight.ttf", 200)
+            drawer = ImageDraw.Draw(image)
+            if len(a)==4: #oстаток 2
+                drawer.text((480,520), a, font=font, fill='black')
+            elif len(a) == 5: #oстаток 3 
+                drawer.text((425,520), a, font=font, fill='black')
+            elif len(a) == 6: #oстаток 4
+                drawer.text((370,520), a, font=font, fill='black')
+            elif len(a)==3: #oстаток 1
+                drawer.text((520,520), a, font=font, fill='black')
+            else: #oстаток 0
+                drawer.text((590,520), a, font=font, fill='black')
+            photo ='fol/new_img-'+'valde'+'_'+str(random.randint(0,6000))+'.jpg'
+            image.save(photo)
+            bot.send_photo(message.from_user.id, open(photo,'rb'))
 bot.polling()
